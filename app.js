afc5b0c67805c2f9f5aafb03477cd76bab5a9f92
Miguel <miguelvd314@gmail.com>
2023-11-22 11:11:21 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+require('dotenv').config()
+
+const app = express()
+
+app.get('/', (req, res) => {
+  console.log('Petición recibida')
+
+  res.status(200).send('<h1>Hola mundo!!!</h1>')
+})
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`)
+})
